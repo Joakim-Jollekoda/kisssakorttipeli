a22e5f4dba1322a60d50ef311a6f518255b2b4f4
SnaksiXD <aleksin.roskat@gmail.com>
2023-10-05 18:15:55 +0300

    vastustajan kortti

--- Joakim-Kissakorttipeli/src/App.jsx
+++ Joakim-Kissakorttipeli/src/App.jsx
@@ -8,15 +8,31 @@ const playerCard = {
                                       ]
 }
 
+const opponentCard = {
+  image:"http://placekitten.com/120/100",
+  stats: [{name:"cuteness", value:63},
+          {name:"speed" , value: 73}
+                                      ]
+}
+
 export default function App(){
+
+function compareCards(){
+  console.log("button clicked!");
+}
+
   return(
     <>
     
     <h1>Hello world!</h1>
+    <div className="game">
     <Card card={playerCard}/>
-
-    </>
+    <button onClick={compareCards} type="button">Play</button>
+    <Card card={opponentCard}/>
+</div>
     
+   </>
+   
   
 
   );
